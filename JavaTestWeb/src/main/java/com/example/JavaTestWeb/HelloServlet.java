598ecb6aa963528f146a837a720f95753d8bf525
yanjing <yj01939@sohu.com>
2024-08-15 19:12:57 +0800

    second commit

--- JavaTestWeb/src/main/java/com/example/JavaTestWeb/HelloServlet.java
+++ JavaTestWeb/src/main/java/com/example/JavaTestWeb/HelloServlet.java
@@ -23,6 +23,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        out.println("second>");
     }
 
     public void destroy() {
